7e7140d69be173232d09cc2ec4727bfbb698be8a
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-28 20:23:23 -0300

    Começando com React Native(App React Native Marcação de Consultas EC)

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.texto}>JapaLindo</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+    texto: {
+      fontSize: 30 ,
+      color: '#FFFF',
+      backgroundColor:'#FF00FF',
+      fontWeight: 'bold',
+    }
+});
